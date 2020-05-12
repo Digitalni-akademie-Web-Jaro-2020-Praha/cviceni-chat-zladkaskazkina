f01a60294b6da26d88af70ba27ed1fa3a8d26dc1
Vladi Benesova <vladi.benesova1@gmail.com>
2020-05-12 13:58:20 +0200

    first version of chat

--- index.js
+++ index.js
@@ -5,15 +5,35 @@
 const messagesElement = document.querySelector('#messages');
 
 const renderMessage = (name, message, date) => {
-  // @TODO: funkce vracející HTML zprávy
+  const messageElm = document.createElement("div");
+  messageElm.className = "card mt-3 mb-3";
+  messageElm.innerHTML = `
+  <div class="card-body">
+    <h5 class="card-title">
+      ${name}
+      <small class="text-muted">${date}</small>
+    </h5>
+    <p class="card-text">${message}</p>
+  </div>`;
+  return messageElm;
 };
 
 const renderMessages = (messages) => {
-  // @TODO: funkce vypisující zprávy na stránku
+  const messagesDiv = document.getElementById("messages");
+  messagesDiv.innerHTML = "";
+
+  for (let i = 0; i < messages.length; i++) {
+    const itemElm = renderMessage(messages[i]);
+
+    messagesDiv.appendChild(itemElm);
+  }
 };
 
 const updateMessages = () => {
-  // @TODO: funkce stahující zprávy ze server a přidávající je na stránku
+  fetch('https://czechichat.herokuapp.com/api/list-messages')
+    .then(response => response.json())
+    .then(data => console.log(data.messages))
+  renderMessages(data.messages);
 };
 
 setInterval(updateMessages, 2000); // Každé dvě sekundy zavolá updateMessages
@@ -25,6 +45,13 @@ const messageInputElement = document.querySelector('#message-input');
 
 const onSubmit = (event) => {
   event.preventDefault(); // Zamezí přesměrování na jinou stránku při odesílání formuláře
+  fetch('https://czechichat.herokuapp.com/api/send-message', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: `{"name": ${nameInputElement.value},"message": ${messageInputElement.value}}`
+  })
 
   console.log(
     'Data:',
